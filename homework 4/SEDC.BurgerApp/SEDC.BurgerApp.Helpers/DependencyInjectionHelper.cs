6b144a2be1b3a8b963330e9f79a21647d5514e0e
Bojan <bojanpavlovski22@gmail.com>
2023-07-17 15:29:37 +0200

    BurgerApp uploaded

---
+++ homework 4/SEDC.BurgerApp/SEDC.BurgerApp.Helpers/DependencyInjectionHelper.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SEDC.BurgerApp.DataAccess;
+using SEDC.BurgerApp.DataAccess.EFImplementations;
+using SEDC.BurgerApp.DataAccess.Interfaces;
+using SEDC.BurgerApp.Domain.Burgers;
+using SEDC.BurgerApp.Domain.Orders;
+using SEDC.BurgerApp.Services.Implementations;
+using SEDC.BurgerApp.Services.Interfaces;
+
+namespace SEDC.BurgerApp.Helpers
+{
+    public class DependencyInjectionHelper
+    {
+        public static void InjectRepositories(IServiceCollection services)
+        {
+            services.AddTransient<IRepository<Order>, OrderEFRepository>();
+            services.AddTransient<IRepository<Burger>, BurgerEFRepository>();
+        }
+
+        public static void InjectServices(IServiceCollection services)
+        {
+            services.AddTransient<IOrderService, OrderService>();
+            services.AddTransient<IBurgerService, BurgerService>();
+        }
+
+        public static void InjectDbContext(IServiceCollection services)
+        {
+            services.AddDbContext<BurgerAppDbContext>(options =>
+            options.UseSqlServer("Server=.\\SQLEXPRESS;Database=BurgerAppDb;Trusted_Connection=True;TrustServerCertificate=true"));
+        }
+    }
+}
